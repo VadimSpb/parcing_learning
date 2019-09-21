1a75e2af24e0f6163136a0d19dfa88f27762ea72
VadimSpb <v.mazeyko@gmail.com>
2019-09-21 17:01:42 +0000

    добавил поиск по зарплате

--- scrapingHW3/main.py
+++ scrapingHW3/main.py
@@ -1,9 +1,10 @@
 from hh_parcer import hh_parcer
 from insdb import insert_to_db
-
+from dbsearch import dbsearch
 
 if __name__=='__main__':
     vacs = hh_parcer()
     insert_to_db(vacs)
+    dbsearch()
    
 
